6a7813ff57dbc2c6e1dcb762aa98d96c086710e7
Julli4n <11952329+Julli4n@users.noreply.github.com>
2021-12-19 19:55:48 -0700

    fix

--- src/pages/Module/Utils/GenerateRobloxItemUrl.lua
+++ src/pages/Module/Utils/GenerateRobloxItemUrl.lua
@@ -4,12 +4,34 @@ local Module = {}
 ---@type SettingsModule
 local Settings = mw.loadData("Module:Settings")
 
+---Format a name for an item URL for SEO purposes.
+---@param name string
+---@return string
+function Module.FormatRobloxItemNameForSeo(name)
+    local NewName = name:gsub("'", ""):gsub("%W", "-"):gsub("^-+", ""):gsub("-+$", "")
+
+    local LowerCaseName = NewName:lower()
+    if LowerCaseName:match("^com%d$") or LowerCaseName:match("^lpt%d$") or LowerCaseName == "aux" or LowerCaseName == "prt" or LowerCaseName == "nul" or LowerCaseName == "con" or LowerCaseName == "bin" or LowerCaseName == "" then
+        NewName = "unnamed"
+    end
+
+    return NewName
+end
+
 ---Get an item URL by it's itemType and itemId.
 ---@param itemType "Bundle" | "DevelopAsset" | "AvatarAsset" | "PlaceAsset" | "Badge" | "Pass"
 ---@param itemId number
-function Module.GenerateRobloxItemUrl(itemType, itemId)
+---@param itemName number | void
+---@return string
+function Module.GenerateRobloxItemUrl(itemType, itemId, itemName)
     local ItemTypeInUrl = Settings.RobloxItemTypeToItemTypeInUrl[itemType] or "library"
-    return Settings.RobloxUrl:format(ItemTypeInUrl, itemId)
+    local ItemUrl = ("%s/%s/%s"):format(Settings.RobloxUrl, ItemTypeInUrl, itemId)
+
+    if itemName ~= nil then
+        ItemUrl = ("%s/%s"):format(ItemUrl, Module.FormatRobloxItemNameForSeo(itemName))
+    end
+
+    return ItemUrl
 end
 
 return Module
